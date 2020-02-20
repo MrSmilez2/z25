92f2e2a01058841ba0a0cbd2830a3f130e65caf7
Nemtsov <mrtwister5950@gmail.com>
2020-02-21 01:07:36 +0300

    reworked queu and stack

--- lesson9/homework.py
+++ lesson9/homework.py
@@ -15,66 +15,55 @@ https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
 """
 
 
-class Stack:
-    def __init__(self, stack_size=None):
-        self.stack_size = stack_size
-        self.stack = []
-        self.last_index = 0
+class Node:
 
-    def push(self, value):
-        if self.stack_size is not None and self.last_index >= self.stack_size:
-            raise Exception("Stack is full")
-        self.stack.append(value)
-        self.last_index += 1
-
-    def pop(self):
-        if self.last_index == 0:
-           raise Exception("Empty stack")
-        self.stack.pop()
-        self.last_index -= 1
-
-    def show_stack(self):
-        print(self.stack)
-
-
-class Queue:
-
-    def __init__(self, q_size=None):
-        self.q_size = q_size
-        self.queue = []
+    def __init__(self, size=None):
+        self.size = size
+        self.nature = []
         self.current_index = 0
 
-    def enqueue(self,value):
-        if self.q_size is not None and self.current_index >= self.q_size:
+
+class Stack(Node):
+
+    def push(self, value):
+        if self.size is not None and self.current_index >= self.size:
+            raise Exception("Stack is full")
+        self.nature.append(value)
+        self.current_index += 1
+
+    def pop(self):
+        if self.current_index == 0:
+            raise Exception("Empty stack")
+        self.nature.pop()
+        self.current_index -= 1
+
+
+class Queue(Node):
+
+    def enqueue(self, value):
+        if self.size is not None and self.current_index >= self.size:
             raise Exception("Queue is full")
-        self.queue.append(value)
+        self.nature.append(value)
         self.current_index += 1
 
     def dequeue(self):
         if self.current_index == 0:
             raise Exception("Empty queue")
-        self.queue.pop(0)
+        self.nature.pop(0)
         self.current_index -= 1
 
-    def show_queue(self):
-        print(self.queue)
 
-
-
-
-
-
-if __name__=='__main__':
-
+if __name__ == '__main__':
     new_stack = Stack(5)
     new_stack.push(1)
     new_stack.push(1)
     new_stack.push(1)
     new_stack.push(1)
     new_stack.push("asdad")
-    new_stack.show_stack()
     new_stack.pop()
     new_stack.pop()
+    print(new_stack.nature)
+
     q = Queue()
     q.enqueue(1)
     q.enqueue(1)
@@ -90,14 +79,12 @@ if __name__=='__main__':
     q.enqueue(1)
     q.enqueue('asd')
     q.enqueue(10)
-    q.show_queue()
     q.dequeue()
     q.dequeue()
-    q.show_queue()
-
+    print(q.nature)
     new_q = Queue(3)
     new_q.enqueue(1)
     new_q.enqueue(3)
     new_q.enqueue(14)
-    new_q.show_queue()
-
+    new_q.dequeue()
+    print(new_q.nature)
